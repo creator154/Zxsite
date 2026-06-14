cd2ebcfe347fa35263fa9c4c6a985a9cc7aa5ce5
creator154 <cteator898@gmail.com>
2026-06-15 01:42:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+batches = [
+    {"name": "Yakeen NEET Hindi 2027"},
+    {"name": "Yakeen NEET English 2027"},
+    {"name": "Lakshya NEET 2027"},
+    {"name": "Arjuna NEET 2028"}
+]
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        batches=batches
+    )
 
 @app.route("/batch/<batch>")
 def batch(batch):
