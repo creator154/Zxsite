043caee7ca6c033314eeba0add577000e618739f
creator154 <cteator898@gmail.com>
2026-06-15 01:34:42 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,21 @@
 
+function searchBatch() {
+    let input = document
+        .getElementById("search")
+        .value.toLowerCase();
+
+    let cards =
+        document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+        if (
+            card.innerText
+            .toLowerCase()
+            .includes(input)
+        ) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
+}
