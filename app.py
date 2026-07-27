80bfec181ee0f26943a312097959880302f52660
creator154 <cteator898@gmail.com>
2026-07-27 09:58:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,18 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
-batches = [
-    {"name": "Yakeen NEET Hindi 2027"},
-    {"name": "Yakeen NEET English 2027"},
-    {"name": "Lakshya NEET 2027"},
-    {"name": "Arjuna NEET 2028"}
-]
-
 @app.route("/")
 def home():
+    with open("config.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+
     return render_template(
         "index.html",
-        batches=batches
-    )
-
-@app.route("/batch/<batch>")
-def batch(batch):
-    return render_template(
-        "batch.html",
-        batch=batch
-    )
-
-@app.route("/tests/<batch>")
-def tests(batch):
-    return render_template(
-        "tests.html",
-        batch=batch
-    )
-
-@app.route("/dpp/<batch>")
-def dpp(batch):
-    return render_template(
-        "dpp.html",
-        batch=batch
+        title=data["title"],
+        url=data["url"]
     )
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
